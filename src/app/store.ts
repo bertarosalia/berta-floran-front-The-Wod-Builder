7b77ff7aea920fdd383a766fedd83d920596b53f
Berta Floran <bertafloran@hotmail.com>
2022-09-01 20:56:59 +0200

    export exercise slice and add it into store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import exercisesReducer from "../features/store/exercisesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    exercises: exercisesReducer,
   },
 });
 
