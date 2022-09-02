848d62d0fa2f8f0261f898096dd44ad7fbc00ab5
Berta Floran <bertafloran@hotmail.com>
2022-09-02 16:17:06 +0200

    create colors vars file

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  background: "#2F3E44",
+  footer: "#C7D8D9",
+  inputs: "#DFDADA",
+  buttonBig: "#89BAC7",
+  buttonEdit: "#568281",
+  buttonDelete: "#DED7C9",
+  white: "#FFF",
+};
