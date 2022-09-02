68d96d4f9b96ed13d1de20bc28a78329598a6dcc
Berta Floran <bertafloran@hotmail.com>
2022-09-02 16:28:15 +0200

    created card exercises style

---
+++ src/components/CardExercises/CardExercisesStyled.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { colors } from "../../theme";
+const CardExercisesStyled = styled.div`
+  width: 700px;
+  height: 300px;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+  background-color: ${colors.background};
+  margin: 20px;
+
+  .card__image {
+    object-fit: cover;
+    border-radius: 10px;
+    margin-left: 30px;
+  }
+
+  .card__info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    color: ${colors.white};
+  }
+
+  .info__data {
+    padding-left: 24px;
+    margin-top: 30px;
+  }
+`;
+
+export default CardExercisesStyled;
