6835925c076438b4d2918cff65d8a86c88dd3b1d
Berta Floran <bertafloran@hotmail.com>
2022-09-03 13:24:16 +0200

    call useApi in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import AppStyled from "./AppStyled";
+import useApi from "./hooks/useApi";
 
 const App = (): JSX.Element => {
+  const { getAllExercises } = useApi();
+
+  useEffect(() => {
+    getAllExercises();
+  });
+
   return (
     <>
       <AppStyled></AppStyled>
