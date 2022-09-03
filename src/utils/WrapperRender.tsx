0984fe31a5ac53603ae1a20866415d75e65f233a
Berta Floran <bertafloran@hotmail.com>
2022-09-03 21:18:28 +0200

    delete wrapper render file

--- src/utils/WrapperRender.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Wrapper from "./Wrapper";
-
-const WrapperRender = (view: JSX.Element, ...options: any) =>
-  render(view, { wrapper: Wrapper, ...options });
-
-export * from "@testing-library/react";
-
-export { WrapperRender };
