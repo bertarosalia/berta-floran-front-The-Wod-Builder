c777f1b44b9d244b0fa40a64cea26e2288a9d036
Berta Floran <bertafloran@hotmail.com>
2022-09-04 17:05:12 +0200

    create exercises list page

---
+++ src/pages/ListExercisesPage.tsx/ListExercisePage.tsx
@@ -0,0 +1,10 @@
+import ListExercises from "../../components/ListExercises/ListExercises";
+
+const ListExercisesPage = (): JSX.Element => {
+  return (
+    <>
+      <ListExercises />
+    </>
+  );
+};
+export default ListExercisesPage;
