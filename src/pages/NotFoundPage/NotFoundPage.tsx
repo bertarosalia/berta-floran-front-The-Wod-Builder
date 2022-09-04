2a6f507eda21e8707430b2b564e6c032f2174e6e
Berta Floran <bertafloran@hotmail.com>
2022-09-04 20:57:32 +0200

    create not found error 404

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+const NotFoundPage = () => {
+  return (
+    <div className="image-container">
+      <img
+        className="image-error"
+        src="img/error404.png"
+        alt="Not Found Error 404"
+      />
+    </div>
+  );
+};
+
+export default NotFoundPage;
