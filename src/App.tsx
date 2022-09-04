37f3e0fa24229bb9a8eeed66dcb0fab361923a18
Berta Floran <bertafloran@hotmail.com>
2022-09-04 18:00:43 +0200

    add navigation and router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import ListExercises from "./components/ListExercises/ListExercises";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import ListExercisesPage from "./pages/ListExercisesPage.tsx/ListExercisePage";
 
-const App = () => {
+function App() {
   return (
     <>
-      <ListExercises />
+      <Navigation />
+      <Routes>
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+        <Route path="/" element={<ListExercisesPage />} />
+        <Route path="/exercises" element={<ListExercisesPage />} />
+      </Routes>
     </>
   );
-};
+}
 
 export default App;
