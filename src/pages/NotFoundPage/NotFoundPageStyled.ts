e8ae16b3ebf26982efbb36ede5d919f8bb27ab23
Berta Floran <bertafloran@hotmail.com>
2022-09-06 23:09:33 +0200

    create not found page style

---
+++ src/pages/NotFoundPage/NotFoundPageStyled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const NotFoundPageStyled = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  .logo-not-found {
+    margin-top: 30px;
+    object-fit: cover;
+    width: 500px;
+    height: 500px;
+  }
+`;
