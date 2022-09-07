44de82e2d7805121158f96b143a710f08a31c7fd
Berta Floran <bertafloran@hotmail.com>
2022-09-07 12:32:38 +0200

    fix font-size hover in card buttons

--- src/components/CardExercises/CardExercisesStyled.ts
+++ src/components/CardExercises/CardExercisesStyled.ts
@@ -51,7 +51,7 @@ const CardExercisesStyled = styled.div`
 
       &:hover {
         background-color: inherit;
-        font-size: 18px;
+        font-size: 16px;
       }
     }
 
@@ -70,7 +70,7 @@ const CardExercisesStyled = styled.div`
 
       &:hover {
         background-color: inherit;
-        font-size: 18px;
+        font-size: 16px;
       }
     }
   }
