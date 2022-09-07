82864570784d1f6f112b5e076bfea17534cf8b39
Berta Floran <bertafloran@hotmail.com>
2022-09-08 00:07:03 +0200

    test functionality delete button in card exercises

--- src/components/CardExercises/CardExercises.test.tsx
+++ src/components/CardExercises/CardExercises.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "../../app/store";
 import IExercise from "../../features/interfaces";
 import CardExercises from "./CardExercises";
+import userEvent from "@testing-library/user-event";
+
+let mockDeleteExercise = { deleteExercise: jest.fn() };
+jest.mock("../../hooks/useExercises", () => () => mockDeleteExercise);
 
 describe("Given a Exercises Card component", () => {
   describe("When is instantiated with an exercise", () => {
@@ -11,7 +16,7 @@ describe("Given a Exercises Card component", () => {
       name: "",
       description: "",
       image: "url",
-      id: "",
+      id: "15",
     };
     test("It should show an image with the exercise", () => {
       render(
@@ -32,6 +37,32 @@ describe("Given a Exercises Card component", () => {
 
       expect((cardImage as HTMLImageElement).alt).toBe(testExercise.name);
       expect(cardImage).toBeInTheDocument();
+    });
+    describe("When click on delete button", () => {
+      test("Then it should call the delete exercise function", async () => {
+        render(
+          <Provider store={store}>
+            <BrowserRouter>
+              <CardExercises
+                key={testExercise.id}
+                name={testExercise.name}
+                body={testExercise.body}
+                description={testExercise.description}
+                image={testExercise.image}
+                id={testExercise.id}
+              />
+            </BrowserRouter>
+          </Provider>
+        );
+
+        const iconTrash = screen.getByTestId("icon-trash");
+
+        await userEvent.click(iconTrash);
+
+        expect(iconTrash).toBeInTheDocument();
+
+        await expect(mockDeleteExercise.deleteExercise).toHaveBeenCalled();
+      });
     });
   });
 });
