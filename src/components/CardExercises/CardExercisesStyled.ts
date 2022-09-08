c27bbcabdcef3aa805caba56e5fb318a8fcd4657
Berta Floran <bertafloran@hotmail.com>
2022-09-08 19:06:21 +0200

    fix card exercise style to change list and move description to detail

--- src/components/CardExercises/CardExercisesStyled.ts
+++ src/components/CardExercises/CardExercisesStyled.ts
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 import styledMainTheme from "../../styledMainTheme";
 const CardExercisesStyled = styled.div`
-  width: 800px;
-  height: 300px;
-  display: flex;
-  align-items: center;
-  border-radius: 20px;
-  background-color: ${styledMainTheme.background};
-  margin: 20px;
-  padding-right: 20px;
+  .card__container {
+    width: 500px;
+    height: 300px;
+    display: flex;
+    align-items: center;
+    border-radius: 20px;
+    background-color: ${styledMainTheme.background};
+    margin: 40px;
+    padding-right: 20px;
+  }
 
   .card__image {
     object-fit: cover;
@@ -16,6 +18,8 @@ const CardExercisesStyled = styled.div`
     height: 200px;
     border-radius: 10px;
     margin-left: 30px;
+    margin-right: 20px;
+    cursor: pointer;
   }
 
   .card__info {
@@ -31,8 +35,19 @@ const CardExercisesStyled = styled.div`
 
   .info__data {
     padding-left: 24px;
-    margin-top: 5px;
-    margin-bottom: 5px;
+    margin: 10px 5px;
+    font-size: 20px;
+  }
+
+  .info__data--description {
+    padding-left: 24px;
+    margin: 10px 5px;
+    font-size: 20px;
+    cursor: pointer;
+
+    &:hover {
+      font-weight: bold;
+    }
   }
 
   .card-button {
@@ -40,20 +55,21 @@ const CardExercisesStyled = styled.div`
       border: none;
       outline: none;
       border-radius: 20px;
-      margin: 40px 20px;
+      margin: 40px 30px;
       height: 40px;
-      width: 120px;
+      width: 100px;
       padding: 5px;
       text-align: center;
       font-weight: bold;
-      font-size: 15px;
+      font-size: 17px;
       cursor: pointer;
       color: ${styledMainTheme.white};
       background-color: ${styledMainTheme.buttonEdit};
+      font-family: ${styledMainTheme.primaryFont};
 
       &:hover {
         background-color: inherit;
-        font-size: 16px;
+        font-size: 18px;
         outline: auto;
       }
     }
@@ -62,20 +78,21 @@ const CardExercisesStyled = styled.div`
       border: none;
       outline: none;
       border-radius: 20px;
-      margin: 40px 20px;
+      margin: 40px 30px;
       height: 40px;
-      width: 120px;
+      width: 100px;
       padding: 5px;
       text-align: center;
       font-weight: bold;
-      font-size: 15px;
+      font-size: 17px;
       color: ${styledMainTheme.black};
       background-color: ${styledMainTheme.buttonDelete};
+      font-family: ${styledMainTheme.primaryFont};
       cursor: pointer;
 
       &:hover {
         background-color: inherit;
-        font-size: 16px;
+        font-size: 18px;
         outline: auto;
       }
     }
