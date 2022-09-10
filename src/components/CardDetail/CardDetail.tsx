63633d47acdfa0ef58df9825be1d7f3aed9a4c0c
Berta Floran <bertafloran@hotmail.com>
2022-09-10 23:07:16 +0200

    change card detail title for execise name

--- src/components/CardDetail/CardDetail.tsx
+++ src/components/CardDetail/CardDetail.tsx
@@ -42,7 +42,7 @@ const CardDetail = (): JSX.Element => {
             className="card-detail__button--close"
             value={"X"}
           />
-          <h1 className="card-detail__title">Want to know more</h1>
+          <h1 className="card-detail__title">{name}</h1>
           <img className="card-detail__image" src={image} alt={name} />
           <ul className="card-detail__info" key={id}>
             <li className="info__data">Name: {name}</li>
