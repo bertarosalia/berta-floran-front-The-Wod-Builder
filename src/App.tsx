5c8cedfa08a51dab49f2213cb5cbe48a7c3e583c
Berta Floran <bertafloran@hotmail.com>
2022-09-11 02:18:30 +0200

    add create exercise form to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import CardDetailPage from "./pages/CardDetailPage/CardDetailPage";
+import CreateExerciseFormPage from "./pages/CreateExerciseFormPage.tsx/CreateExerciseFormPage";
 import ListExercisesPage from "./pages/ListExercisesPage/ListExercisePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/exercises" element={<ListExercisesPage />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/detail:id" element={<CardDetailPage />} />
+        <Route path="/create-exercise" element={<CreateExerciseFormPage />} />
       </Routes>
     </>
   );
