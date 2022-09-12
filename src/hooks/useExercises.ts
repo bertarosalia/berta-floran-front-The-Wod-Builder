bfd1463cb020a56e70dc51e01296431b1a688717
Berta Floran <bertafloran@hotmail.com>
2022-09-12 15:52:20 +0200

    refactor hook use exercises

--- src/hooks/useExercises.ts
+++ src/hooks/useExercises.ts
@@ -1,6 +1,5 @@
-import axios from "axios";
 import { useCallback } from "react";
-import { useAppDispatch } from "../app/hooks";
+import { useDispatch } from "react-redux";
 import IExercise from "../features/interfaces";
 import {
   createExerciseActionCreator,
@@ -8,47 +7,42 @@ import {
   loadAllExercisesactionCreator,
 } from "../features/store/exercisesSlice";
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
 const useExercises = () => {
-  const dispatch = useAppDispatch();
+  const dispatch = useDispatch();
+  const apiUrl = process.env.REACT_APP_API_URL;
 
-  const getAllExercises = useCallback(async (): Promise<void> => {
-    const loadExercisesUrl = `${apiUrl}/exercises` as string;
-
+  const getAllExercises = useCallback(async () => {
     try {
-      const {
-        data: { exercises },
-      } = await axios.get(loadExercisesUrl);
+      const response: Response = await fetch(`${apiUrl}/exercises` as string);
+      const data = await response.json();
+      const { exercises } = data;
       dispatch(loadAllExercisesactionCreator(exercises));
-    } catch (error) {}
-  }, [dispatch]);
+    } catch {}
+  }, [apiUrl, dispatch]);
 
   const deleteExercise = async (deleteId: IExercise["id"]) => {
     try {
-      await axios.delete(`${apiUrl}/${deleteId}`);
-
+      await fetch(`${apiUrl}/${deleteId}`, {
+        method: "DELETE",
+      });
       dispatch(deleteExerciseActionCreator(deleteId));
     } catch {}
   };
 
-  const createExercise = useCallback(
-    async (newExercise: IExercise) => {
-      try {
-        const {
-          data: { exerciseCreated },
-        } = await axios.post(`${apiUrl}/create`, newExercise);
-        dispatch(createExerciseActionCreator(exerciseCreated));
-        return exerciseCreated;
-      } catch {}
-    },
-    [dispatch]
-  );
-  return {
-    getAllExercises,
-    deleteExercise,
-    createExercise,
-  };
-};
+  const createExercise = async (newExercise: IExercise) => {
+    try {
+      const response = await fetch(`${apiUrl}/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newExercise),
+      });
+      const { exercise } = await response.json();
 
+      dispatch(createExerciseActionCreator(exercise));
+    } catch {}
+  };
+  return { getAllExercises, deleteExercise, createExercise };
+};
 export default useExercises;
