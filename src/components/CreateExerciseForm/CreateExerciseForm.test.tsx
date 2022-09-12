f0ed19cce64f82bc99d0fd122807026bd7b204f9
Berta Floran <bertafloran@hotmail.com>
2022-09-13 01:36:54 +0200

    test exercise has been created in form create exercise

--- src/components/CreateExerciseForm/CreateExerciseForm.test.tsx
+++ src/components/CreateExerciseForm/CreateExerciseForm.test.tsx
@@ -4,13 +4,14 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../../app/store";
 import CreateExerciseForm from "./CreateExerciseForm";
+import userEvent from "@testing-library/user-event";
 
 beforeEach(() => jest.restoreAllMocks());
+const mockCreate = jest.fn();
 
-const mockUseExercises = {
-  createExercise: jest.fn(),
-};
-jest.mock("../../hooks/useExercises", () => () => mockUseExercises);
+jest.mock("../../hooks/useExercises", () => () => ({
+  createExercise: mockCreate,
+}));
 
 describe("Given a create exercise form component", () => {
   describe("When instantiated", () => {
@@ -32,6 +33,7 @@ describe("Given a create exercise form component", () => {
 
       elements.forEach((element) => expect(element).toBeInTheDocument());
     });
+
     test("When user clicks it should call the useState", async () => {
       const useState = jest.spyOn(React, "useState");
       render(
@@ -45,6 +47,39 @@ describe("Given a create exercise form component", () => {
       screen.getByRole("button", { name: "CREATE" });
 
       await waitFor(() => expect(useState).toHaveBeenCalled());
+    });
+  });
+
+  describe("When user type in all required inputs", () => {
+    test("Then it should be created a new exercise", async () => {
+      const name = "snatch";
+      const body = "arms";
+      const description = "jjbbhbh";
+      const image = "url";
+
+      const textButton = "CREATE";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <CreateExerciseForm />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const resultName = screen.getByLabelText("Name");
+      const resultBody = screen.getByLabelText("Body");
+      const resultDescription = screen.getByLabelText("Description");
+      const resultImage = screen.getByLabelText("Image");
+      const button = screen.getByRole("button", { name: textButton });
+
+      await userEvent.type(resultName, name);
+      await userEvent.type(resultBody, body);
+      await userEvent.type(resultDescription, description);
+      await userEvent.type(resultImage, image);
+      await userEvent.click(button);
+
+      expect(mockCreate).toHaveBeenCalled();
     });
   });
 });
