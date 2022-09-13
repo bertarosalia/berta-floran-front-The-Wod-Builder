b89e4bb01a84dd280a3ede7a22dd4c9c3b265ede
Berta Floran <bertafloran@hotmail.com>
2022-09-14 00:06:50 +0200

    remove loading from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { useAppSelector } from "./app/hooks";
-import Loading from "./components/Loading/Loading";
 import Navigation from "./components/Navigation/Navigation";
 import CardDetailPage from "./pages/CardDetailPage/CardDetailPage";
 import CreateExerciseFormPage from "./pages/CreateExerciseFormPage.tsx/CreateExerciseFormPage";
@@ -8,11 +6,8 @@ import ListExercisesPage from "./pages/ListExercisesPage/ListExercisePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
-  const { isLoadingShowing } = useAppSelector((state) => state.ui);
   return (
     <>
-      {isLoadingShowing && <Loading />}
-
       <Navigation />
       <Routes>
         <Route path="/" element={<Navigate to="/exercises" />} />
