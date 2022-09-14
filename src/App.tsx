1ea6dc6e34201e179d19cb0dab3dc47e048d176c
Berta Floran <bertafloran@hotmail.com>
2022-09-14 12:21:29 +0200

    add hot toast to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,21 @@ import CardDetailPage from "./pages/CardDetailPage/CardDetailPage";
 import CreateExerciseFormPage from "./pages/CreateExerciseFormPage.tsx/CreateExerciseFormPage";
 import ListExercisesPage from "./pages/ListExercisesPage/ListExercisePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const { isLoadingShowing } = useAppSelector((state) => state.ui);
   return (
     <>
       {isLoadingShowing && <Loading />}
+      <Toaster position="bottom-center" />
 
       <Navigation />
       <Routes>
         <Route path="/" element={<Navigate to="/exercises" />} />
         <Route path="/exercises" element={<ListExercisesPage />} />
         <Route path="*" element={<NotFoundPage />} />
-
+        <Route path="/not-found-error" element={<NotFoundPage />} />
         <Route path="/detail/:id" element={<CardDetailPage />} />
 
         <Route path="/create-exercise" element={<CreateExerciseFormPage />} />
