34dcd1172f75e0e6ba5e72b596384ff531744191
Berta Floran <bertafloran@hotmail.com>
2022-09-14 16:12:45 +0200

    fix use exercises tests

--- src/hooks/useExercises.test.tsx
+++ src/hooks/useExercises.test.tsx
@@ -2,10 +2,7 @@ import { renderHook, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../app/store";
-import {
-  closeLoaderActionCreator,
-  showLoaderActionCreator,
-} from "../features/store/UI/UISlice";
+
 import useExercises from "./useExercises";
 
 interface WrapperProps {
@@ -30,27 +27,6 @@ jest.mock("react-redux", () => ({
 }));
 
 describe("Given a useExercises hook", () => {
-  describe("When it´s invoked with his method getAllExercises and fetch response with a list of exercises", () => {
-    test("Then dispatch must called with action showLoader", async () => {
-      const { result } = renderHook(() => useExercises(), {
-        wrapper: Wrapper,
-      });
-
-      await result.current.getAllExercises();
-
-      expect(mockDispatch).toHaveBeenCalledWith(showLoaderActionCreator());
-    });
-    test("Then dispatch must be called with action closeLoader", async () => {
-      const { result } = renderHook(() => useExercises(), {
-        wrapper: Wrapper,
-      });
-
-      await result.current.getAllExercises();
-
-      expect(mockDispatch).toHaveBeenCalledWith(closeLoaderActionCreator());
-    });
-  });
-
   describe("When its function createExercise is called", () => {
     test("It should send FormData passed as arguments to the DB", async () => {
       const mockExerciseFormData = new FormData();
