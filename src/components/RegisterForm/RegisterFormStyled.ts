80da88965f4a7b9069bf81bc7eeb4ac12646a8fd
Berta Floran <bertafloran@hotmail.com>
2022-09-20 22:54:19 +0200

    create register form styled

---
+++ src/components/RegisterForm/RegisterFormStyled.ts
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import styledMainTheme from "../../styledMainTheme";
+
+const RegisterFormStyled = styled.section`
+  justify-content: center;
+
+  .register-form__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .register-form__title {
+    color: ${styledMainTheme.white};
+    text-align: center;
+    font-size: 25px;
+  }
+
+  .register-form__input {
+    width: 800px;
+    height: 40px;
+    background-color: ${styledMainTheme.inputs};
+    font-family: ${styledMainTheme.primaryFont};
+    font-size: 15px;
+    font-weight: bolder;
+    border-radius: 10px;
+    margin: 10px 0 20px;
+    cursor: text;
+    text-align: center;
+  }
+
+  .repeat_password {
+    width: 800px;
+    height: 40px;
+    background-color: ${styledMainTheme.inputs};
+    font-family: ${styledMainTheme.primaryFont};
+    font-size: 15px;
+    font-weight: bolder;
+    border-radius: 10px;
+    margin: 10px 0 20px;
+    cursor: text;
+    text-align: center;
+  }
+
+  .create-form__label {
+    color: ${styledMainTheme.white};
+    font-size: 18px;
+  }
+
+  .register-user__button {
+    border-radius: 10px;
+    border: none;
+    outline: none;
+    height: 40px;
+    width: 100px;
+    padding: 5px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 15px;
+    font-family: ${styledMainTheme.primaryFont};
+    background-color: ${styledMainTheme.buttonBig};
+    cursor: pointer;
+    margin: 20px;
+
+    &:hover {
+      background-color: inherit;
+      outline: auto;
+    }
+  }
+
+  .register-form__inputs--container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .register-form__button {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-left: 190px;
+  }
+`;
+
+export default RegisterFormStyled;
