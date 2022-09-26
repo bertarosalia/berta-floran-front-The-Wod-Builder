a8bb69d338a217eaec6ae42b2c2e6c4da058f13d
Berta Floran <bertafloran@hotmail.com>
2022-09-26 20:34:49 +0200

    test user login function

--- src/hooks/useUsers/useUsers.test.ts
+++ src/hooks/useUsers/useUsers.test.ts
@@ -1,34 +1,51 @@
 import { renderHook } from "@testing-library/react";
-import toast from "react-hot-toast";
 import { ProtoUser } from "../../features/store/users/model/user";
+import toast from "react-hot-toast";
+import { loginUserActionCreator } from "../../features/store/users/userSlice";
 import useUser from "./useUsers";
 
+const mockDispatch = jest.fn();
+const mockNavigate = jest.fn();
+jest.mock("react-hot-toast");
 let user: ProtoUser;
 
-beforeEach(() => {
-  user = {
-    name: "Manuela",
-    email: "manuela@manuela.com",
-    password: "12345",
-    repeat_password: "12345",
-  };
-});
-
-jest.mock("react-hot-toast");
-const mockUseDispatch = jest.fn();
-
-jest.mock("../../app/hooks", () => ({
-  ...jest.requireActual("../../app/hooks"),
-  useAppDispatch: () => mockUseDispatch,
+const mockuserWithToken = {
+  token: "token",
+  id: "",
+  password: "12345",
+  email: "",
+};
+jest.mock("../../utils/auth/auth", () => () => mockuserWithToken);
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
 }));
-
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 interface WrapperProps {
   children: JSX.Element | JSX.Element[];
 }
-
 let Wrapper: ({ children }: WrapperProps) => JSX.Element;
 
+// Wrapper = ({ children }: WrapperProps): JSX.Element => {
+//   return (
+//     <Provider store={store}>
+//       <BrowserRouter>{children}</BrowserRouter>
+//     </Provider>
+//   );
+// };
 describe("Given the use users custom hook", () => {
+  beforeEach(() => {
+    user = {
+      name: "pepe",
+      email: "pepe@pepe.com",
+      password: "11345",
+      repeat_password: "11345",
+    };
+  });
+
   describe("When use register is called with a valid user", () => {
     test("Then should call success modal", async () => {
       const successModalMessage =
@@ -47,6 +64,7 @@ describe("Given the use users custom hook", () => {
         position: "top-center",
       });
     });
+
     describe("When user register it´s called with an invalid user", () => {
       test("Then it should call the error modal", async () => {
         const errorUser = {
@@ -69,6 +87,61 @@ describe("Given the use users custom hook", () => {
         expect(toast.error).toHaveBeenCalledWith(errorModalMessage, {
           duration: 5000,
           position: "top-center",
+        });
+      });
+    });
+
+    describe("When login user is called", () => {
+      const loggedUser = {
+        email: "berta@berta.com",
+        password: "123456",
+      };
+      test("Then should call the dispatch login and a user", async () => {
+        const {
+          result: {
+            current: { userLogin },
+          },
+        } = renderHook(useUser, { wrapper: Wrapper });
+
+        await userLogin(loggedUser);
+
+        expect(mockDispatch).toHaveBeenCalledWith(
+          loginUserActionCreator(mockuserWithToken)
+        );
+      });
+      test("It should call the success modal", async () => {
+        const {
+          result: {
+            current: { userLogin },
+          },
+        } = renderHook(useUser, { wrapper: Wrapper });
+
+        await userLogin(loggedUser);
+
+        expect(toast.success).toHaveBeenCalledWith("Let´s train!", {
+          duration: 5000,
+          position: "top-center",
+        });
+      });
+      describe("And it´s called with an invalid user", () => {
+        test("Then error modal has called with message 'Uh no! You are not logged yet. Try again!'", async () => {
+          const {
+            result: {
+              current: { userLogin },
+            },
+          } = renderHook(useUser, { wrapper: Wrapper });
+
+          mockuserWithToken.password = "45687";
+
+          await userLogin(mockuserWithToken);
+
+          expect(toast.error).toHaveBeenCalledWith(
+            "Uh no! You are not logged yet. Try again!",
+            {
+              duration: 5000,
+              position: "top-center",
+            }
+          );
         });
       });
     });
