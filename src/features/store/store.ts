0b8e57c1096c7225cbc0875be8f08602a0680f59
Berta Floran <bertafloran@hotmail.com>
2022-09-26 14:36:50 +0200

    add user reducer to store

--- src/features/store/store.ts
+++ src/features/store/store.ts
@@ -1,11 +1,13 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import exercisesReducer from "./exercises/exercisesSlice";
 import { UIReducer } from "./UI/UISlice";
+import { userReducer } from "./users/userSlice";
 
 export const store = configureStore({
   reducer: {
     exercisesReducer: exercisesReducer,
     ui: UIReducer,
+    user: userReducer,
   },
 });
 
