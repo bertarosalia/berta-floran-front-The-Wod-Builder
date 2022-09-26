24a453751243b16ab7f756eb003df4e8e44c7198
Berta Floran <bertafloran@hotmail.com>
2022-09-26 15:06:28 +0200

    add test for logout reducer

--- src/features/store/users/userSlice.test.ts
+++ src/features/store/users/userSlice.test.ts
@@ -2,24 +2,30 @@ import { userLoginState } from "./model/user";
 import { userReducer } from "./userSlice";
 
 describe("Given a user reducer function", () => {
-  const initialUser = {
-    isLogged: false,
-    user: {
-      name: "",
-    },
-  };
   describe("When it´s invoked with login user and an invalid state or action", () => {
     test("Then it should return the initial state", () => {
+      const previousUserState: userLoginState = {
+        isLogged: false,
+        user: {
+          name: "",
+        },
+      };
       const wrongState = undefined;
       const unknownAction = { type: "" };
 
       const userReducersReturn = userReducer(wrongState, unknownAction);
 
-      expect(userReducersReturn).toStrictEqual(initialUser);
+      expect(userReducersReturn).toStrictEqual(previousUserState);
     });
   });
-  describe("When it´s called with previousUIState and a payload with an user", () => {
+  describe("When it´s called with previousUserState and action login user", () => {
     test("Then it should return the property isLogged true with user data", () => {
+      const previousUserState: userLoginState = {
+        isLogged: false,
+        user: {
+          name: "",
+        },
+      };
       const userPayload = {
         type: "user/loginUser",
         payload: { name: "Manuela" },
@@ -30,7 +36,30 @@ describe("Given a user reducer function", () => {
         user: { name: "Manuela" },
       };
 
-      const newState = userReducer(initialUser, userPayload);
+      const newState = userReducer(previousUserState, userPayload);
+
+      expect(newState).toStrictEqual(newStateExpected);
+    });
+  });
+  describe("Whe it´s called logout user reducer with previousState and action logout user", () => {
+    test("Then it should return the initial state", () => {
+      const previousUserState: userLoginState = {
+        isLogged: true,
+        user: {
+          name: "Manuela",
+        },
+      };
+      const userPayload = {
+        type: "user/logoutUser",
+      };
+      const newStateExpected: userLoginState = {
+        isLogged: false,
+        user: {
+          name: "",
+        },
+      };
+
+      const newState = userReducer(previousUserState, userPayload);
 
       expect(newState).toStrictEqual(newStateExpected);
     });
