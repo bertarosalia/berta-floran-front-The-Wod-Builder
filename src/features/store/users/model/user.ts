46ae1147de6e89e90a36d200470f78a4f0994054
Berta Floran <bertafloran@hotmail.com>
2022-09-26 14:35:38 +0200

    add user and user login state interfaces

--- src/features/store/users/model/user.ts
+++ src/features/store/users/model/user.ts
@@ -4,3 +4,14 @@ export interface ProtoUser {
   password: string;
   repeat_password: string;
 }
+
+export interface User {
+  name: string;
+}
+
+export interface userLoginState {
+  isLogged: boolean;
+  user: {
+    name: string;
+  };
+}
