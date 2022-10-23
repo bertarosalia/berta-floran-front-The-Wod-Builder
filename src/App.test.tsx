7465747b85a4d2cd70ecdbe9bba07bcae3e0c9d3
Berta Floran <bertafloran@hotmail.com>
2022-10-23 15:15:15 +0200

    fix App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,31 +3,10 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
-import { UIState } from "./features/store/UI/model/UI";
-import { UIReducer } from "./features/store/UI/UISlice";
-
-const mockedFetchValue = [
-  {
-    exercises: [
-      {
-        body: "",
-        name: "",
-        description: "",
-        image: "",
-        id: "",
-      },
-    ],
-  },
-];
-
-jest.mock("react-redux", () => ({
-  ...jest.requireActual("react-redux"),
-  useSelector: () => mockedFetchValue,
-}));
 
 describe("Given an App component", () => {
-  describe("When is instantiated with a page not found", () => {
-    test("It should render an image with the name 'Page Not Found 404'", () => {
+  describe("When is instantiated", () => {
+    test("It should render an image with name 'The Wod Builder Logo'", () => {
       render(
         <>
           <Provider store={store}>
@@ -37,36 +16,10 @@ describe("Given an App component", () => {
           </Provider>
         </>
       );
-    });
-    describe("When it's instantiated", () => {
-      test("Then should call show loader reducer with property is loading showing to true", () => {
-        render(
-          <Provider store={store}>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </Provider>
-        );
 
-        const previousUIPayload: UIState = {
-          isLoadingShowing: false,
-          message: "",
-          type: true,
-        };
-        const UIPayload = {
-          type: "ui/showLoader",
-        };
+      const navLink = screen.getByRole("img", { name: "The Wod Builder Logo" });
 
-        const expectedNewUI = {
-          isLoadingShowing: true,
-          message: "",
-          type: true,
-        };
-
-        const newUI = UIReducer(previousUIPayload, UIPayload);
-
-        expect(newUI).toStrictEqual(expectedNewUI);
-      });
+      expect(navLink).toBeInTheDocument();
     });
   });
 });
