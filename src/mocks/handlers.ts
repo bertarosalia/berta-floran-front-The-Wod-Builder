a839836f4992e3d66e4afea87cc3ee0a6eab0942
Berta Floran <bertafloran@hotmail.com>
2022-10-23 21:17:10 +0200

    add ids in handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,9 @@ import { rest } from "msw";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
-const exerciseId: string = "232464fe42536dd232";
+const exerciseId: string = "6321a224a3fa8622ce45e644";
+
+const wrongId: string = "12345";
 
 export const handlers = [
   rest.get(
@@ -27,12 +29,14 @@ export const handlers = [
               body: "arms",
               description: "great exercise",
               image: "url",
+              id: "1234",
             },
             {
               name: "lungees",
               body: "legs",
               description: "legs die",
               image: "url",
+              id: "1235",
             },
           ],
         })
@@ -53,7 +57,7 @@ export const handlers = [
     }
   ),
   rest.delete(
-    `${process.env.REACT_APP_API_URL}exercises/wrongId`,
+    `${process.env.REACT_APP_API_URL}exercises/"${wrongId}"`,
     async (req, res, ctx) => {
       return res(ctx.status(404), ctx.json({ error: "Exercise not found" }));
     }
@@ -76,13 +80,13 @@ export const handlers = [
   ),
 
   rest.get(
-    `${process.env.REACT_APP_API_URL}exercises/wrongId`,
+    `${process.env.REACT_APP_API_URL}exercises/${wrongId}`,
     (_req, res, ctx) => {
       return res(ctx.status(404), ctx.json({ error: "Something went wrong" }));
     }
   ),
   rest.post(
-    `${process.env.REACT_APP_API_URL}exercises/`,
+    `${process.env.REACT_APP_API_URL}exercises/create`,
     async (req, res, ctx) => {
       const headerTestError = req.headers.get("IsTestError");
 
@@ -102,6 +106,7 @@ export const handlers = [
           name: "push press",
           description: "very great exercise",
           image: "url",
+          id: "123",
         })
       );
     }
