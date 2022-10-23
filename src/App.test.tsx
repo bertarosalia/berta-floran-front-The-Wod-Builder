0fdc7f21a456c253fbfddd8a118c77235bec7499
Berta Floran <bertafloran@hotmail.com>
2022-10-23 15:26:28 +0200

    add test to App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
 
 describe("Given an App component", () => {
   describe("When is instantiated", () => {
-    test("It should render an image with name 'The Wod Builder Logo'", () => {
+    test("It should render an image with name 'The Wod Builder Logo' in navigation bar", () => {
       render(
         <>
           <Provider store={store}>
@@ -20,6 +20,25 @@ describe("Given an App component", () => {
       const navLink = screen.getByRole("img", { name: "The Wod Builder Logo" });
 
       expect(navLink).toBeInTheDocument();
+    });
+  });
+
+  describe("When is instantiated with create exercise", () => {
+    test("It should show a button with name 'CREATE'", () => {
+      const pathTest = "/create-exercise";
+      render(
+        <>
+          <Provider store={store}>
+            <MemoryRouter initialEntries={[pathTest]}>
+              <App />
+            </MemoryRouter>
+          </Provider>
+        </>
+      );
+
+      const button = screen.getByRole("button", { name: "CREATE" });
+
+      expect(button).toBeInTheDocument();
     });
   });
 });
