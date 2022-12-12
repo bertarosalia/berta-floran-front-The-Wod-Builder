56b949e12c708f8db2c9f55641325195489eeabe
Berta Floran <bertafloran@hotmail.com>
2022-12-12 21:49:51 +0100

    add loading.mp4 and change it for gif

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,7 +4,7 @@ const Loading = (): JSX.Element => {
   return (
     <>
       <LoadingStyled>
-        <img src="img/loading.gif" alt="loading..." />
+        <img src="img/loading.mp4" alt="loading..." />
         <span>Loading...</span>
       </LoadingStyled>
     </>
